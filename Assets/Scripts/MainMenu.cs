bb7dd0c4bdb3cad2f98e62c438a493264b44096c
dougstanish <dougstanish@gmail.com>
2018-04-25 16:56:00 -0400

    Adds main menu and splash screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public bool quitButton;
+
+    // Use this for initialization
+    void Start () {
+
+        Button btn = gameObject.GetComponent<Button>();
+
+        if (quitButton)
+        {
+            btn.onClick.AddListener(TaskOnClickQuit);
+        }
+        else
+        {
+            btn.onClick.AddListener(TaskOnClickLoad);
+        }
+    }
+
+    void TaskOnClickLoad()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    void TaskOnClickQuit()
+    {
+        print("Quit");
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-Initial Commit to test proccess
